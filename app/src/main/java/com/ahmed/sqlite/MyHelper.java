b8f6fd30e7205f8bdca7371611ecff8c22c1fbd6
AhmedRaafat2 <chemist.ahmedraafat@gmail.com>
2018-11-07 18:34:40 +0200

    3rd Update

--- app/src/main/java/com/ahmed/sqlite/MyHelper.java
+++ app/src/main/java/com/ahmed/sqlite/MyHelper.java
@@ -2,8 +2,11 @@ package com.ahmed.sqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class MyHelper  extends SQLiteOpenHelper{
 
@@ -48,6 +51,33 @@ public class MyHelper  extends SQLiteOpenHelper{
         //3
         db.insert(TABLE_NAME,null,values);
 
+    }
 
+    public ArrayList<Contact> getAllContacts(){
+        ArrayList<Contact> contacts = new ArrayList<>();
+        String SELECT_QUERY = "SELECT * FROM "+TABLE_NAME+"";
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(SELECT_QUERY,null);
+
+        //علشان محتاجه جوا ال do While
+        Contact contact = null;
+
+        //
+        if(cursor.moveToFirst()){
+            do{
+                String name = cursor.getString(cursor.getColumnIndex(KEY_NAME));
+                String phone = cursor.getColumnName(cursor.getColumnIndex(KEY_PHONE));
+                int id = cursor.getInt(cursor.getColumnIndex(KEY_ID));
+
+                //contact object
+                contact = new Contact(id,name,phone);
+
+                //Array
+                contacts.add(contact);
+            }while (cursor.moveToNext());
+        }
+        //return ArrayList
+        return contacts;
     }
 }
